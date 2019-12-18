a1d16158967a7ca5c7b5450e78c758d4ded45e83
Joshua Ob <joshuaaob@gmail.com>
2019-12-18 11:31:34 +0000

    Test date filter

--- tests/integration/components/invoice-list-test.js
+++ tests/integration/components/invoice-list-test.js
@@ -1,7 +1,5 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
-import wait from 'ember-test-helpers/wait';
 import EmberObject from '@ember/object';
 
 module('Integration | Component | invoice-list', function(hooks) {
@@ -10,30 +8,17 @@ module('Integration | Component | invoice-list', function(hooks) {
     EmberObject.create({ id: 1, amount: 150, date: "2019-12-12" }),
     EmberObject.create({ id: 2, amount: 230, date: "2019-12-14" })
   ];
-  const FILTERED_INVOICES = [
-    EmberObject.create({ id: 1, amount: 150, date: "2019-12-12" }),
-  ];
 
-  test('it shows all invoices', async function(assert) {
-    this.set('invoices', INVOICES);
-    this.render(hbs`
-      {{invoice-list invoices=invoices}}
-    `);
-
-    return wait().then(() => {
-      assert.equal(this.$('.collection-item').length, 2);
-    });
+  test('should filter by date when present', function(assert) {
+    const invoiceList = this.owner.lookup('component:invoice-list');
+    invoiceList.set("invoices", INVOICES);
+    invoiceList.set("filteredDate", "2019-12-12");
+    assert.equal(invoiceList.get('invoicesFilteredByDate').length, 1);
   });
 
-  test('it shows filtered invoices', async function(assert) {
-    this.set('invoices', FILTERED_INVOICES);
-    this.set('filteredDate', "2019-12-12");
-    this.render(hbs`
-      {{invoice-list invoices=invoices}}
-    `);
-
-    return wait().then(() => {
-      assert.equal(this.$('.collection-item').length, 1);
-    });
+  test('should return all invoices when date filter is not present', function(assert) {
+    const invoiceList = this.owner.lookup('component:invoice-list');
+    invoiceList.set("invoices", INVOICES);
+    assert.equal(invoiceList.get('invoicesFilteredByDate').length, 2);
   });
 });
